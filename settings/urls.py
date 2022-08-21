b73038b5f12e87415662fa0ceaa0579c70bbb9e9
Farruxnet <32985353+Farruxnet@users.noreply.github.com>
2022-08-21 19:39:47 +0500

    Update urls.py

--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-# 12
+# 123
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app.urls')),
